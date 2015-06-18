0235b03b06dab9f986724233d3d9c15886bea811
Sho <soboy2@users.noreply.github.com>
2015-06-18 06:36:52 -0400

    FP scraping 1

--- htmlparser.py
+++ htmlparser.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import requests
 
 #r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
 #r.status_code
@@ -8,7 +9,11 @@ from bs4 import BeautifulSoup
 
 s = requests.Session()
 
+for yr in range(2010, 2015):
+    r = s.get("http://www.fantasypros.com/nfl/reports/leaders/ppr.php?year=" + str(yr), auth=('user', 'pwd'))
+    f = open("fantasy_fb_leaders_" + str(yr) + ".html", "w")
+    f.write(r.text)
 
-r = s.get("http://www.transtats.bts.gov/Data_Elements.aspx?Data=2")
 
-soup = BeautifulSoup(r.text)
+
+#soup = BeautifulSoup(r.text)
