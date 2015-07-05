ea525545315257e672b662af47055c211b4905d8
Sho <sho.asod@gmail.com>
2015-07-04 23:26:18 -0400

    update another script

--- htmlparser_stats.py
+++ htmlparser_stats.py
@@ -5,7 +5,7 @@ s = requests.Session()
 
 for yr in range(2012, 2015):
     for wk in range(1, 18):
-        r = s.get("http://www.fantasypros.com/nfl/reports/leaders/ppr.php?year="+ str(yr) + "&week=" + str(wk), auth=('sho', 'V1Os0MdNHvCfgKr'))
+        r = s.get("http://www.fantasypros.com/nfl/reports/leaders/ppr.php?year="+ str(yr) + "&week=" + str(wk), auth=('usr', 'pwd'))
         week_html = "fantasy_fb_stats_" + str(yr) + "_wk" + str(wk) + ".html"
         f = open(week_html, "w")
         f.write(r.text)
